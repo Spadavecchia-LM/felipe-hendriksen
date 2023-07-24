b3aa1072b0e1ca123c124250a0f7497bfd540482
Spadavecchia-LM <leonardo.spadavecchia@hotmail.com>
2023-07-24 18:26:56 -0300

    animaciones a tarjetas de trabajo

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const publications = [
         description: `Poem “Nuestras Nadas Poco Difieren“ published in <em>lit.202</em>, Issue Three, Autumn 2022`,
         url: "https://lit202.org/read/hendriksen-nuestras-nadas-poco-difieren",
         date: "July 24, 2022",
-        image:"https://res.cloudinary.com/leoms96/image/upload/v1690143755/felipe/nuestras%20nadas.jpg"
+        image: "https://res.cloudinary.com/leoms96/image/upload/v1690143755/felipe/nuestras%20nadas.jpg"
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ const publications = [
         description: `Flash fiction piece “Trying to Explain a Dream I Had to My Psychologist” published in <em>Press Pause Press</em>, Volume 6, April 2022.`,
         url: "https://www.presspausepress.org/felipe-h",
         date: "",
-        image:"https://res.cloudinary.com/leoms96/image/upload/v1690143755/felipe/psicologo.webp"
+        image: "https://res.cloudinary.com/leoms96/image/upload/v1690143755/felipe/psicologo.webp"
     },
     {
         id: 3,
@@ -21,36 +21,40 @@ const publications = [
         description: `Flash fiction piece “Objectivity in Art” published in Coastal Shelf, Spring Annual #1, 2022.`,
         url: "https://coastalshelf.com/spring-annual-2022-objectivity-in-art-by-felipe-rodolfo-hendriksen",
         image: "https://res.cloudinary.com/leoms96/image/upload/v1690143755/felipe/art.jpg"
-    },{
-        id:4,
+    }, {
+        id: 4,
         title: "The Unexpected Witness",
         description: `Flash fiction piece “The Unexpected Witness” published in <em>Press Pause Press</em>, Volume 6, April 2022.`,
         url: "https://www.presspausepress.org/felipe-h",
-        date:"",
-        image:"https://res.cloudinary.com/leoms96/image/upload/v1690143755/felipe/unexpected%20witness.png"
+        date: "",
+        image: "https://res.cloudinary.com/leoms96/image/upload/v1690143755/felipe/unexpected%20witness.png"
 
     }
-        
+
 
 ]
 const experience = [
     {
-        id: 1,
+        id: "exp-1",
         task: "Teaching Assistant",
         where: "Pontifica Universidad Católica Argentina",
-        time: `<em>Present</em>`
+        time: "Present",
+        description: "LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR"
     },
     {
-        id: 2,
+        id: "exp-2",
         task: "Part-time language and literature teacher",
         where: "Freelance",
-        time: `<em>Present</em>`
+        time: `Present`,
+        description: "asdasdadsadsadsadadsadsadsada"
+
     },
     {
-        id: 3,
+        id: "exp-3",
         task: "Undergraduate Teaching Assistant",
         where: "Washington College - Chestertown, MD, United States of America",
-        time: `<em>From 01/2019 to 05/2019`
+        time: `From 01/2019 to 05/2019`,
+        description: ""
     }
 ]
 
@@ -65,23 +69,23 @@ const typed = new Typed(introText, {
     showCursor: false,
     loop: false,
 })
-const workButtons = document.querySelectorAll(".btn_work")
+
 
 introText.innerHTML = ""
 
 
 
-function navBar(){
+function navBar() {
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active")
         navMenu.classList.toggle("active")
     })
-    
+
     navLinks.forEach(link => {
         link.addEventListener("click", () => {
             hamburger.classList.remove("active")
             navMenu.classList.remove("active")
-    
+
         })
     })
 }
@@ -103,23 +107,25 @@ function renderPublications(arr) {
 function renderExperience(arr) {
     arr.forEach(element => {
         document.querySelector(".work_cards_container").innerHTML += `
-        <div class="work_card">
+        <div class="work_card" id=${element.id}>
             <h2>${element.task}</h2>
-            <button class="btn_work">View Full Info</button>
+            <h4>${element.where}</h4>
+            <span>${element.time}</span>
+            <button class="btn_work" id=${element.id}>View Full Info</button>
         </div>
 
         `
     })
 }
 setTimeout(() => {
-document.querySelector("#aboutBtn").classList.remove("hidden")
-anime({
-    targets:document.querySelector("#aboutBtn") ,
-    opacity: ["0%","100%"],
-    duration:5000,
-    direction: 'normal',
-    loop: false
-  });
+    document.querySelector("#aboutBtn").classList.remove("hidden")
+    anime({
+        targets: document.querySelector("#aboutBtn"),
+        opacity: ["0%", "100%"],
+        duration: 5000,
+        direction: 'normal',
+        loop: false
+    });
 }, 4100)
 
 
@@ -128,3 +134,29 @@ renderExperience(experience)
 renderPublications(publications)
 navBar()
 
+const workButtons = document.querySelectorAll(".btn_work")
+
+function viewWorkInfo() {
+    workButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            const id = event.target.id
+            console.log(id)
+            const card = document.querySelector(`#${id}`)
+
+            const workDescription = experience.filter(exp => exp.id === id)
+
+            card.innerHTML = ""
+            anime({
+                targets: card,
+                rotateY: "360",
+            });
+            setTimeout(() => {
+                card.innerHTML = `
+                <p>${workDescription[0].description}</p>
+                `
+            }, 250)
+
+        })
+    })
+}
+viewWorkInfo()
